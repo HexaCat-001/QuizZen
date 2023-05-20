6bcfbb84e3521882ccd6b08dee8e8a9d35f168aa
Suman Karmakar <sumankarmakar.sam@gmail.com>
2023-05-21 00:35:55 +0530

    style: add style to Quizz div

---
+++ script.js
@@ -0,0 +1,35 @@
+const QuizDB = [
+    {
+        question: "Q1: What is the fullform of HTML?",
+        a: "Hi Text Markup Law",
+        b: "Hello To My Land",
+        c: "HyperText Markup Language",
+        d: "Hypertext Markup Language",
+        ans: "ans4"
+    },
+    {
+        question: "Q2: What is the fullform of HTML?",
+        a: "Hi Text Markup Law",
+        b: "Hello To My Land",
+        c: "HyperText Markup Language",
+        d: "Hypertext Markup Language",
+        ans: "ans4"
+    },
+    {
+        question: "Q3: What is the fullform of HTML?",
+        a: "Hi Text Markup Law",
+        b: "Hello To My Land",
+        c: "HyperText Markup Language",
+        d: "Hypertext Markup Language",
+        ans: "ans4"
+    },
+    {
+        question: "Q4: What is the fullform of HTML?",
+        a: "Hi Text Markup Law",
+        b: "Hello To My Land",
+        c: "HyperText Markup Language",
+        d: "Hypertext Markup Language",
+        ans: "ans4"   
+    }
+]
+
